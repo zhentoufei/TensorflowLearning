5e6ca650d844b5397d1d877097f37a6c2ab537f4
zhentoufei <zhentoufei@163.com>
2017-10-16 17:15:01 +0800

    在tensorflow中定义网络层的添加，完成训练

--- 莫烦Python的学习/6.tensorflow添加层def addlayer.py
+++ 莫烦Python的学习/6.tensorflow添加层def addlayer.py
@@ -6,11 +6,12 @@ __software__ = 'PyCharm'
 __file__ = '6.tensorflow添加层def addlayer.py'
 
 import tensorflow as tf
+import numpy as np
 
 
 def add_layer(inputs, in_size, out_size, activation_fun=None):
     Weights = tf.Variable(tf.random_uniform([in_size, out_size]))  # 我们在这里定义的时候，如果变量是矩阵，我们就大写首字母
-    biases = tf.Variable(tf.zeros(1, out_size) + 0.1)
+    biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
     Wx_plus_b = tf.matmul(inputs, Weights) + biases
     if activation_fun is None:
         outputs = Wx_plus_b
@@ -20,4 +21,24 @@ def add_layer(inputs, in_size, out_size, activation_fun=None):
 
 
 if __name__ == '__main__':
-    pass
+    x_data = np.linspace(-1, 1, 300)[:, np.newaxis]
+    noise = np.random.normal(0, 0.05, x_data.shape)
+    y_data = np.square(x_data) - 0.5 + noise
+
+    xs = tf.placeholder(tf.float32, [None, 1])  # 这里的None表示无论给出多少个样本都是可以的哦
+    ys = tf.placeholder(tf.float32, [None, 1])
+
+    l1 = add_layer(xs, 1, 10, activation_fun=tf.nn.relu)
+    prediction = add_layer(l1, 10, 1, activation_fun=None)
+    loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction),
+                                        reduction_indices=[1]))
+    train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+
+    init = tf.initialize_all_variables()
+    sess = tf.Session()
+    sess.run(init)
+
+    for i in range(10000):
+        sess.run(train_step, feed_dict={xs: x_data, ys: y_data})
+        if i % 50 == 0:
+            print(sess.run(loss, feed_dict={xs: x_data, ys: y_data}))
