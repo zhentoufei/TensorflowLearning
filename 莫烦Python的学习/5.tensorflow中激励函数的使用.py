2974b9d978fbc21f57277d828c82ca2657b65466
zhentoufei <zhentoufei@163.com>
2017-10-16 14:36:14 +0800

    在tensorflow中激活函数

---
+++ 莫烦Python的学习/5.tensorflow中激励函数的使用.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Mr.Finger'
+__date__ = '2017/10/16 14:26'
+__site__ = ''
+__software__ = 'PyCharm'
+__file__ = '5.tensorflow中激励函数的使用.py'
+
+# 然而这节课只讲了在tensorflow中有哪些激活函数，并没有设计的代码编写
+
+if __name__ == '__main__':
+    pass
